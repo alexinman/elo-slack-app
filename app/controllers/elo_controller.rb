ff30618e39423ebba592c78d91a2726fc09ef401
Alex Inman <alex.inman@introhive.com>
2019-06-12 15:13:35 -0300

    Fix extra spacing in no rated games message

--- app/controllers/elo_controller.rb
+++ app/controllers/elo_controller.rb
@@ -114,7 +114,7 @@ class EloController < ApplicationController
     players_rel = Player.where(team_id: current_team).where("user_id like ?", "%#{user_id}%").includes(:game_type)
     players_rel = players_rel.joins(:game_type).where(game_types: {game_type: type}) if type.present?
     players = players_rel.take(20)
-    reply "#{user_id == current_user ? "You haven't" : "<#{user_id}> hasn't"} played any ELO rated games #{type.present? ? "for #{type}" : ""} yet." and return if players.empty?
+    reply "#{user_id == current_user ? "You haven't" : "<#{user_id}> hasn't"} played any ELO rated games #{type.present? ? "for #{type} " : ""}yet." and return if players.empty?
     attachments = players.map do |player|
       games = player.games.includes(:player_one, :player_two).order(:created_at).last(5).reverse
       fields = [
