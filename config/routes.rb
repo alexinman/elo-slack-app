272373e6c8fcbafc21e9be4e13030a52b039bb9a
Stephen Downward <stephen.downward@introhive.com>
2019-06-14 14:59:15 -0300

    start of basic readonly api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   post '/elo', :controller => 'elo', :action => 'elo'
 end
+Rails.application.routes.draw do
+  get '/history', :controller => 'webapi', :action => 'history'
+end
+Rails.application.routes.draw do
+  get '/leaderboard', :controller => 'webapi', :action => 'leaderboard'
+end
+Rails.application.routes.draw do
+  get '/gametypes', :controller => 'webapi', :action => 'gametypes'
+end
